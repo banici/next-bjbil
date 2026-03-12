fddb04cc10f590250dd8730631fc3352d5e358f0
banici <banicworld@gmail.com>
2026-03-12 11:12:11 +0100

    removed unused font import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,7 @@
 import type { Metadata } from "next";
-import { Inter, Nunito_Sans } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/Footer/Footer";
 import HeaderWrapper from "./components/Header/HeaderWrapper";
-
-const inter = Inter({
-  subsets: ["latin"],
-  weight: ["400", "600", "700"],
-});
-
-const nunitoSans = Nunito_Sans({
-  subsets: ["latin"],
-  weight: ["400", "600", "700"],
-});
 
 export const metadata: Metadata = {
   title: "Företagsnamn – Hem",
